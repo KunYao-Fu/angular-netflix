6a7a2ffea713e949b81aee6fc760d6c3ca90e3ff
kidom22771935 <kidom22771935@gmail.com>
2020-11-02 11:31:31 +0800

    develop sign up9

--- src/app/modules/guest/pages/login/login.component.ts
+++ src/app/modules/guest/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '@auth/auth.service';
 import { FirebaseService } from '@services/firebase.service';
 
@@ -14,21 +15,40 @@ export class LoginComponent implements OnInit {
     private $fb: FirebaseService,
     public router: Router,
     public $auth: AuthService,
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
+    this.initial();
   }
-  
-  public isShowTerms = false
-  public isShowSignUpGoogle = false
-  public account
-  public password
-  public index = 0
 
-  public submit() {
-    console.log(this.account)
-    this.index = this.index + 1
-    this.$fb.document('users', 'user' + this.index).create({ account: this.account, password: this.password })
+  public isShowTerms = false
+  public auth: FormGroup;
+
+  public getErrorMsg(field: string) {
+    const FIELD = this.auth.get(field);
+    return FIELD.hasError('required') ? '必填欄位' :
+      FIELD.hasError('invalid') ? FIELD.getError('invalid').message : null;
+  }
+
+  public signUp() {
+    this.router.navigate(['/guest']);
+  }
+
+  private initial() {
+    this.auth = this.formBuilder.group({
+      email: [this.route.snapshot.paramMap.get('email'), [Validators.required, this.validateEmail]],
+      password: ['', [Validators.required]]
+    })
+  }
+
+  public login() {
+    this.$auth.loginByEmail(this.auth.getRawValue());
+  }
+
+  private validateEmail(control: AbstractControl): ValidationErrors {
+    return /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/.test(control.value) ? null : { invalid: { message: 'test' } };
   }
 
 }
